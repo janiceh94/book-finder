57c691f3b3760102852f6e5e19c7a1ea5b403271
janiceh94 <haesung94@gmail.com>
2022-09-22 15:35:16 -0700

    yarn add react-icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {userState} from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import {HiOutlineMenuAlt3} from 'react-icons/hi';
 
 const Navbar = () => {
     return (
