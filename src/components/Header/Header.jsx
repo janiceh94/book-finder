415cb99cef98e90cfbd813a45532ee06856eb81f
janiceh94 <haesung94@gmail.com>
2022-09-22 15:20:53 -0700

    added to classNames

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
         <div className="holder">
             <header className="header">
                 <Navbar/>
-                <div className="header-content">
-                    <h2 className="header-title">
+                <div className="header-content flex flex-c text-center text-white">
+                    <h2 className="header-title text-capitalize">
                         Search for your book
                     </h2>
                     <br/>
-                    <p className="header-text">
+                    <p className="header-text fs-18 fw-3">
                         lorem ipsum dolor sit amet, consectetur adip
                     </p>
                     <SearchBar/>
