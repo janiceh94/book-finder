844c2bff4af6dc91683006120ef957f852fd63c6
janiceh94 <haesung94@gmail.com>
2022-09-22 15:41:04 -0700

    added some styling to navbar and outline menu button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import React, {userState} from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import {HiOutlineMenuAlt3} from 'react-icons/hi';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);const handleNavbar = () => setToggleMenu(!toggleMenu);
+
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className = 'navbar' id = 'navbar'>
+            <div className = 'container navbar-content flex'>
+                <div className = 'brand-and-toggler flex flex-sb'>
+                    <Link to ='/' className = 'navbar-brand flex'>
+                        <span className = 'text-uppercase fw-7 fs-24 ls-1'>
+                            BookFinder
+                        </span>
+                    </Link>
+                    <button type = 'button' className = 'navbar-toggler-btn' onClick={handleNavbar}>
+                        <HiOutlineMenuAlt3 size = {35} style = {{color: `${toggleMenu ? "#fff" : "#010101"}`}}/>
+                    </button>
+                </div>
+            </div>
+        </nav>
     )
 }
 
