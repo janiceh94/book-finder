bca1b6ba0d4781d994175ee09ce33f2b659334fa
janiceh94 <haesung94@gmail.com>
2022-09-21 22:30:06 -0700

    added content to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import SearchBar from '../SearchBar/SearchBar';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="holder">
+            <header className="header">
+                <Navbar/>
+                <div className="header-content">
+                    <h2 className="header-title">
+                        Search for your book
+                    </h2>
+                    <br/>
+                    <p className="header-text">
+                        lorem ipsum dolor sit amet, consectetur adip
+                    </p>
+                    <SearchBar/>
+                </div>
+            </header>
         </div>
     )
 }
