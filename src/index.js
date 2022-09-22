c0e75296d7a2dec537c6eaa24b16f3ae587db6cf
janiceh94 <haesung94@gmail.com>
2022-09-21 22:14:59 -0700

    import pages and components to index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import BrowserRouter from 'react-router-dom'
 import Routes from 'react-router-dom'
 import Route from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import BookList from './components/BookList/BookList'
+import BookDetails from './components/BookDetails/BookDetails'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
       <Routes>
-        
+        <Route path ='/' element = {<Home/>}/>
+        <Route path = 'about' element = {<About/>}/>
+        <Route path = 'book' element = {<BookList/>}/>
+        <Route path = '/book/:id' element = {<BookDetails/>}/>
       </Routes>
     </React.StrictMode>
   </BrowserRouter>
