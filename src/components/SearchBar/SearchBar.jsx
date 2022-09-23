3c1c1a88a33b79505a885ba93a2fb07f1d5847a8
janiceh94 <haesung94@gmail.com>
2022-09-22 22:26:20 -0700

    styled and created searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import './SearchBar.css';
+
 
 const SearchBar = () => {
     return (
-        <div>
-            Searchbar
+        <div className="search-form">
+            <div className='container'>
+                <div className = 'search-form-content'>
+                    <form className="search-form">
+                        <div className = 'search-form-elem flex flex-sb bg-white'>
+                            <input type = 'text' className = 'form-control' placeholder = 'Book Title'/>
+                            <button type = 'submit' className = 'flex flex-c'>
+                                <FaSearch className = 'text-purple' size={32}/>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
