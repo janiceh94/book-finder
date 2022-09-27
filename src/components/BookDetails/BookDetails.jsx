bee813f49c97cc3d610741ebeb7ec22218d48be6
janiceh94 <haesung94@gmail.com>
2022-09-26 19:23:22 -0700

    book details

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -51,6 +51,31 @@ const BookDetails = () => {
                     <button type ='button' className='flex flex-c back-btn' onClick={() => navigate('/book')}><FaArrowLeft size = {22}/>
                     <span className='fs-18 fw-6'>Go Back</span>
                     </button>
+                    <div className='book-details-content grid'>
+                        <div className='book-details-img'>
+                            <img src = {book ?.cover_img} alt = 'cover img'/>
+                        </div>
+                        <div className='book-details-info'>
+                            <div className='book-details-item title'>
+                                <span className='fw-6 fs-24'>{book ?.title}</span>
+                            </div>
+                            <div className='book-details-item description'>
+                                <span className='fw-6 fs-24'>{book ?.description}</span>
+                            </div>
+                            <div className='book-details-item'>
+                                <span className='fw-6'>Subject Place: </span>
+                                <span className='text-italic'>{book ?.subject_places}</span>
+                            </div>
+                            <div className='book-details-item'>
+                                <span className='fw-6'>Subject Times: </span>
+                                <span className='text-italic'>{book ?.subject_times}</span>
+                            </div>
+                            <div className='book-details-item'>
+                                <span className='fw-6'>Subjects: </span>
+                                <span className='text-italic'>{book ?.subjects}</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
